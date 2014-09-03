45eafdadb4b3215935c93f1a9782564717e91d6b
Gerald Villorente <geraldvillorente@gmail.com>
2014-09-03 15:47:02 +0800

    Remove class as it is useless

--- modules/angular_view/all-articles.tpl.php
+++ modules/angular_view/all-articles.tpl.php
@@ -1,6 +1,6 @@
 <div id="article" ng-controller="articlecontroller">
   <!-- Iterate the result. Equivalent to foreach in php. -->
-  <div class="cat" ng-repeat="article in articles">
+  <div ng-repeat="article in articles">
     <h2>{{article.node.title}}</h2>
     <div class="body">{{article.node.Body}}</div>
     <div class="image"><img src="{{article.node.Image}}" /></div>
